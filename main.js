0230cf1f89ee60e992920ba635d35af2edf732f9
Alex Mckenzie <86716054+Alezie29@users.noreply.github.com>
2023-04-08 15:58:25 +0700

    added 16x16 grid

---
+++ main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('#container')
+const gridSize = 16;
+
+for (let i = 0; i < gridSize * gridSize; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    container.appendChild(cell);
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.style.width = `${100 / gridSize}%`;
+    cell.style.height = `${100 / gridSize}%`;
+});
+
